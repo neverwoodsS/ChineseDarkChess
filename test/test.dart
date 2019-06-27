de54970be976d5332bc70d6b17e7b69ec0ddc89a
zhanglulin <413700858@qq.com>
2019-06-27 16:16:45 +0800

    complete role test

--- test/test.dart
+++ test/test.dart
@@ -3,47 +3,14 @@ import 'package:ttk_chess/game/location.dart';
 import 'package:ttk_chess/role/role.dart';
 
 void main() {
-//  Game game = Game.shuffle(Mode.NORMAL);
-//
-//  for (List<Role> roles in game.roles) {
-//    print("\n");
-//
-//    var sb = StringBuffer();
-//    for (Role role in roles) {
-//      sb.write(role.getLabel());
-//    }
-//
-//    print(sb.toString());
-//  }
-
-//  game.rule = 1;
-//
-//  Role r1 = Turret();
-//  game.addRoleToLocation(r1, Location(0, 2));
-//
-//  Role r2 = Pawn();
-//  game.addRoleToLocation(r2, Location(3, 2));
-//
-//  Role r3 = Pawn();
-//  r3.side = true;
-//  r3.hide = false;
-//  game.addRoleToLocation(r3, Location(2, 2));
-//
-//  Role r4 = King();
-//  r4.side = false;
-//  r4.hide = false;
-//  game.addRoleToLocation(r4, Location(1, 2));
-//
-//  print(r1.canEat(r2, game));
-//  print(r3.canEat(r4, game));
-//  print(r4.canEat(r3, game));
-
   testRoute();
 }
 
 testRoute() {
+  var startLocation = Location(0, 0);
+
   var soldier = Soldier()
-    ..location = Location(0, 0)
+    ..location = startLocation
   ;
   assert(null == soldier.routeTo(Location(0, 2)));
   assert(null == soldier.routeTo(Location(1, 1)));
@@ -51,10 +18,57 @@ testRoute() {
   assert(null != soldier.routeTo(Location(0, 1)));
   assert(null != soldier.routeTo(Location(1, 0)));
 
-  var rook = Rook()
-    ..location = Location(0, 0)
+  var king = King("")
+    ..location = startLocation
   ;
+  assert(null == king.routeTo(Location(0, 2)));
+  assert(null == king.routeTo(Location(1, 1)));
+  assert(null == king.routeTo(Location(2, 0)));
+  assert(null != king.routeTo(Location(0, 1)));
+  assert(null != king.routeTo(Location(1, 0)));
+
+  var knight = Knight()
+    ..location = startLocation
+  ;
+  assert(null == knight.routeTo(Location(0, 0)));
+  assert(null == knight.routeTo(Location(1, 0)));
+  assert(null == knight.routeTo(Location(0, 1)));
+  assert(null == knight.routeTo(Location(1, 1)));
+  assert(null != knight.routeTo(Location(1, 2)));
+  assert(null != knight.routeTo(Location(2, 1)));
+
+  var minister = Minister()
+    ..location = startLocation
+  ;
+  assert(null == minister.routeTo(Location(0, 0)));
+  assert(null == minister.routeTo(Location(1, 0)));
+  assert(null == minister.routeTo(Location(0, 1)));
+  assert(null == minister.routeTo(Location(1, 1)));
+  assert(null == minister.routeTo(Location(1, 2)));
+  assert(null == minister.routeTo(Location(2, 1)));
+  assert(null != minister.routeTo(Location(2, 2)));
+
+  var guard = Guard()
+    ..location = startLocation
+  ;
+  assert(null == guard.routeTo(Location(0, 0)));
+  assert(null == guard.routeTo(Location(0, 1)));
+  assert(null == guard.routeTo(Location(1, 0)));
+  assert(null != guard.routeTo(Location(1, 1)));
+
+  var rook = Rook()
+    ..location = startLocation
+  ;
+  assert(null == rook.routeTo(Location(0, 0)));
+  assert(null == rook.routeTo(Location(1, 1)));
   assert(3 == rook.routeTo(Location(4, 0)).length);
   assert(5 == rook.routeTo(Location(0, 6)).length);
-  assert(null == rook.routeTo(Location(1, 1)));
+
+  var cannon = Cannon()
+    ..location = startLocation
+  ;
+  assert(null == cannon.routeTo(Location(0, 0)));
+  assert(null == cannon.routeTo(Location(1, 1)));
+  assert(6 == cannon.routeTo(Location(7, 0)).length);
+  assert(8 == cannon.routeTo(Location(0, 9)).length);
 }
