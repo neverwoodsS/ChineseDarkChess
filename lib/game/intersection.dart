0ecc4489f46e8a63092c9442c41aaa24f253e6bf
zhanglulin <413700858@qq.com>
2019-06-27 18:23:27 +0800

    create const class BattleField

--- lib/game/intersection.dart
+++ lib/game/intersection.dart
@@ -3,14 +3,14 @@ import 'battle_field.dart';
 
 class Intersection {
 
-  final _min = BattleField.DESERT;
-  final _max = BattleField.SIZE - BattleField.DESERT - 1;
+  static const _MIN = BattleField.DESERT;
+  static const _MAX = BattleField.SIZE - BattleField.DESERT - 1;
 
   final int x, y;
   Role role;
   bool desert;
 
   Intersection(this.x, this.y) {
-    desert = (x < _min || x > _max) && (y < _min || y > _max);
+    desert = (x < _MIN || x > _MAX) && (y < _MIN || y > _MAX);
   }
 }
